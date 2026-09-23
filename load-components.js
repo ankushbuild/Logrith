50f2af170a298d963d520b24d8c3adb23cd847a0
Ankush Build's <268417877+ankushbuild@users.noreply.github.com>
2026-09-23 14:51:31 +0530

    Implement AdSense script loading
    Added AdSense script loading functionality.

--- load-components.js
+++ load-components.js
@@ -6,6 +6,15 @@ function loadComponent(id, file) {
     });
 }
 
+// AdSense
+(function() {
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2892592901355098";
+  s.crossOrigin = "anonymous";
+  document.head.appendChild(s);
+})();
+
 // no language detect, direct load
 loadComponent("navbar", "/components/navbar.html");
 loadComponent("footer", "/components/footer.html");
